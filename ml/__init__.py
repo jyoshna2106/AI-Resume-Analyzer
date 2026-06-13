87e691a5b7c862bd90267bbeed022c8f85f80322
jyoshna2106 <jyoshnajyo199@gmail.com>
2026-06-13 19:12:04 +0530

    Add files via upload

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,2 +1,16 @@
-from .callbacks import create_models_with_nvtx_range  # noqa: F401
-from .models import *  # noqa: F401, F403
+from ...language import BaseDefaults, Language
+from .lex_attrs import LEX_ATTRS
+from .stop_words import STOP_WORDS
+
+
+class MalayalamDefaults(BaseDefaults):
+    lex_attr_getters = LEX_ATTRS
+    stop_words = STOP_WORDS
+
+
+class Malayalam(Language):
+    lang = "ml"
+    Defaults = MalayalamDefaults
+
+
+__all__ = ["Malayalam"]
