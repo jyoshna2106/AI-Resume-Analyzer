5bfffe84dab47b91f1270b11cb857d710245290c
jyoshna2106 <jyoshnajyo199@gmail.com>
2026-06-13 18:30:37 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,7 +1,45 @@
-#!/bin/env python
-#Copyright ReportLab Europe Ltd. 2000-2017
-#see license.txt for license details
-#history https://hg.reportlab.com/hg-public/reportlab/log/tip/src/reportlab/lib/__init__.py
-__version__='3.3.0'
-import os
-RL_DEBUG = 'RL_DEBUG' in os.environ
+from .attributeruler import AttributeRuler
+from .dep_parser import DependencyParser
+from .edit_tree_lemmatizer import EditTreeLemmatizer
+from .entity_linker import EntityLinker
+from .entityruler import EntityRuler
+from .functions import merge_entities, merge_noun_chunks, merge_subtokens
+from .lemmatizer import Lemmatizer
+from .morphologizer import Morphologizer
+from .ner import EntityRecognizer
+from .pipe import Pipe
+from .sentencizer import Sentencizer
+from .senter import SentenceRecognizer
+from .span_finder import SpanFinder
+from .span_ruler import SpanRuler
+from .spancat import SpanCategorizer
+from .tagger import Tagger
+from .textcat import TextCategorizer
+from .textcat_multilabel import MultiLabel_TextCategorizer
+from .tok2vec import Tok2Vec
+from .trainable_pipe import TrainablePipe
+
+__all__ = [
+    "AttributeRuler",
+    "DependencyParser",
+    "EditTreeLemmatizer",
+    "EntityLinker",
+    "EntityRecognizer",
+    "EntityRuler",
+    "Morphologizer",
+    "Lemmatizer",
+    "MultiLabel_TextCategorizer",
+    "Pipe",
+    "SentenceRecognizer",
+    "Sentencizer",
+    "SpanCategorizer",
+    "SpanFinder",
+    "SpanRuler",
+    "Tagger",
+    "TextCategorizer",
+    "Tok2Vec",
+    "TrainablePipe",
+    "merge_entities",
+    "merge_noun_chunks",
+    "merge_subtokens",
+]
